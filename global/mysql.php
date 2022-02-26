8224fbb77a7b137ed1d509766756771f7f56beb6
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-26 16:25:29 -0500

    testing db for production

--- global/mysql.php
+++ global/mysql.php
@@ -1,29 +1,20 @@
 <?php
-//Get Heroku ClearDB connection information
-if (getenv('APP_ENV') == 'production') {
-  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-  $cleardb_server = $cleardb_url["host"];
-  $cleardb_username = $cleardb_url["user"];
-  $cleardb_password = $cleardb_url["pass"];
-  $cleardb_db = substr($cleardb_url["path"], 1);
-  $active_group = 'default';
-  $query_builder = TRUE;
-  // Connect to DB
-  //$conn = new PDO ($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-  $conn = new PDO("mysql:host=$cleardb_server;dbname=$$cleardb_db", $cleardb_username, $cleardb_password);
-} else {  {
-
-    $db_config = parse_ini_file(".env");
-    $host = $db_config["DB_HOST"];
-    $db_username = $db_config["DB_USERNAME"];
-    $db_password = $db_config["DB_PASSWORD"];
-    $db_name = $db_config["DB_NAME"];
-  }
-  try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_username, $db_password);
-    //$conn = mysqli_connect($host, $db_username, $db_password, $db_db);
-  } catch (PDOException $e) {
-    echo "Connection to the database failed: " . $e->getMessage();
-  }
-}
-?>
+      session_start();
+      $db_config = parse_ini_file(".env"); 
+      $environment = 'production';
+      if($environment  == 'production'){
+        $host = 'us-cdbr-east-05.cleardb.net';
+        $db_username = 'bbbf8000c05b1f';
+        $db_password = 'a0c45315';
+        $db_name= 'heroku_8e7c6bf016d5cda';
+      } else {
+        // $host = $db_config["DB_HOST"];
+        // $db_username = $db_config["DB_USERNAME"];
+        // $db_password = $db_config["DB_PASSWORD"];
+        // $db_name= $db_config["DB_NAME"];
+      }
+      try{
+        $conn = new PDO ("mysql:host=$host;dbname=$db_name", $db_username, $db_password);
+      } catch (PDOException $e) {
+        echo "Connection to the database failed: " . $e->getMessage();
+      }
