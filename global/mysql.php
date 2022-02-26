fb3e514107b5ca9661ae3541a21b43781e5c13a7
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-26 16:36:11 -0500

    added clear db configuration

--- global/mysql.php
+++ global/mysql.php
@@ -1,17 +1,18 @@
 <?php
       session_start();
       $db_config = parse_ini_file(".env"); 
-      $environment = 'production';
-      if($environment  == 'production'){
-        $host = 'us-cdbr-east-05.cleardb.net';
-        $db_username = 'bbbf8000c05b1f';
-        $db_password = 'a0c45315';
-        $db_name= 'heroku_8e7c6bf016d5cda';
+      $isProduction = true; // keep false in local
+      $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      if($isProduction){
+        $host = $cleardb_url["host"];
+        $db_username = $cleardb_url["user"];
+        $db_password = $cleardb_url["pass"];
+        $db_name= substr($cleardb_url["path"],1);
       } else {
-        // $host = $db_config["DB_HOST"];
-        // $db_username = $db_config["DB_USERNAME"];
-        // $db_password = $db_config["DB_PASSWORD"];
-        // $db_name= $db_config["DB_NAME"];
+        $host = $db_config["DB_HOST"];
+        $db_username = $db_config["DB_USERNAME"];
+        $db_password = $db_config["DB_PASSWORD"];
+        $db_name= $db_config["DB_NAME"];
       }
       try{
         $conn = new PDO ("mysql:host=$host;dbname=$db_name", $db_username, $db_password);
