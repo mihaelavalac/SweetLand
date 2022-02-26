c4db65a9b27be63823675a7366e43752c46055cd
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-26 14:05:42 -0500

    connecting to ClearDB

--- global/mysql.php
+++ global/mysql.php
@@ -1,36 +1,15 @@
-<!-- <?php
-// session_start();
-// $server_name = "localhost";
-// $db_username = "root";
-// $db_password = "";
-// try{
-//   $conn = new PDO("mysql:host=$server_name;dbname=sweetland_db", $db_username, $db_password);
-// } catch (PDOException $e) {
-//   echo "Connection to the database failed: " . $e->getMessage();
-// } ?> -->
-
-
 <?php
 session_start();
-
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$db_config = parse_ini_file(".env"); 
 
 
-// $server_name = "localhost";
-// $db_username = "root";
-// $db_password = "";
-// try{
-//   $conn = new PDO("mysql:host=$server_name;dbname=sweetland_db", $db_username, $db_password);
-// } catch (PDOException $e) {
-//   echo "Connection to the database failed: " . $e->getMessage();
-// } 
-?>
+$host = $db_config["DB_HOST"];
+$db_username = $db_config["DB_USERNAME"];
+$db_password = $db_config["DB_PASSWORD"];
+$db_name= $db_config["DB_NAME"];
+try{
+  $conn = new PDO("mysql:host=$host;dbname=$db_name", $db_username, $db_password);
+} catch (PDOException $e) {
+  echo "Connection to the database failed: " . $e->getMessage();
+} ?>
+
